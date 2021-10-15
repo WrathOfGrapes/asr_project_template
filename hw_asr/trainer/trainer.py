17c22e6f61d028d7ef7841a422799ebc2212a683
WrathOfGrapes <i8yourson@gmail.com>
2021-10-15 13:53:56 +0300

    Fixed _log_predictions (didn't test it though)

--- hw_asr/trainer/trainer.py
+++ hw_asr/trainer/trainer.py
@@ -205,13 +205,13 @@ class Trainer(BaseTrainer):
         # TODO: implement logging of beam search results
         if self.writer is None:
             return
-        predictions = log_probs.cpu().argmax(-1)
-        pred_texts = [self.text_encoder.ctc_decode(p) for p in predictions]
-        argmax_pred_texts = [
+        argmax_predictions = log_probs.cpu().argmax(-1)
+        argmax_predictions = [
             self.text_encoder.decode(p)[: int(l)]
-            for p, l in zip(predictions, log_probs_length)
+            for p, l in zip(argmax_predictions, log_probs_length)
         ]
-        tuples = list(zip(pred_texts, text, argmax_pred_texts))
+        predicted_texts = [self.text_encoder.ctc_decode(p) for p in argmax_predictions]
+        tuples = list(zip(predicted_texts, text, argmax_predictions))
         shuffle(tuples)
         to_log_pred = []
         to_log_pred_raw = []
