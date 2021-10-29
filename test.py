f973e7217bc34a341c214f70c7ce9b760b303340
Kirill Gelvan <kirill-gelvan@mail.ru>
2021-10-29 21:50:01 +0300

    add cpu support via map_location in test.py

--- test.py
+++ test.py
@@ -18,7 +18,10 @@ DEFAULT_CHECKPOINT_PATH = ROOT_PATH / "default_test_model" / "checkpoint.pth"
 
 def main(config, out_file):
     logger = config.get_logger("test")
-
+    
+    # define cpu or gpu if possible
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
     # text_encoder
     text_encoder = CTCCharTextEncoder.get_simple_alphabet()
 
@@ -30,14 +33,13 @@ def main(config, out_file):
     logger.info(model)
 
     logger.info("Loading checkpoint: {} ...".format(config.resume))
-    checkpoint = torch.load(config.resume)
+    checkpoint = torch.load(config.resume, map_location=device)
     state_dict = checkpoint["state_dict"]
     if config["n_gpu"] > 1:
         model = torch.nn.DataParallel(model)
     model.load_state_dict(state_dict)
 
     # prepare model for testing
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = model.to(device)
     model.eval()
 
