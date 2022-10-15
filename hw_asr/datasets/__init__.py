6e225929feda4732bbb49f7a16a24a703944736e
Yulia Korotkova <y.korotkova@just-ai.com>
2022-10-15 03:12:35 +0300

    update init

--- hw_asr/datasets/__init__.py
+++ hw_asr/datasets/__init__.py
@@ -2,10 +2,12 @@ from hw_asr.datasets.custom_audio_dataset import CustomAudioDataset
 from hw_asr.datasets.custom_dir_audio_dataset import CustomDirAudioDataset
 from hw_asr.datasets.librispeech_dataset import LibrispeechDataset
 from hw_asr.datasets.ljspeech_dataset import LJspeechDataset
+from hw_asr.datasets.common_voice import CommonVoiceDataset
 
 __all__ = [
     "LibrispeechDataset",
     "CustomDirAudioDataset",
     "CustomAudioDataset",
-    "LJspeechDataset"
+    "LJspeechDataset",
+    "CommonVoiceDataset"
 ]
